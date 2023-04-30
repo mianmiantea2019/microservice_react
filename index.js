291868aed26a4783d9a87fb9694c50a075162d83
mianmiantea2019 <sguo321@gatech.edu>
2023-04-29 23:26:27 -0400

    fix ES6 syntax

--- index.js
+++ index.js
@@ -7,19 +7,30 @@ const db = [];
 
 app.post('/posts', (req, res) => {
   const { title } = req.body;
-
   if (!title || title.trim() === '') {
     return res.status(400).json({ error: 'Title is required' });
   }
-
   const id = Date.now().toString();
   const post = {
     id,
     title
   };
-  
   db.push(post);
   return res.status(201).send(post);
 });
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+  
+  res.status(201).send(comments);
+});
+
 
 export default app;
--- test/posts.test.js
+++ test/posts.test.js
@@ -1,5 +1,5 @@
-const request = require('supertest');
-const app = require('../index').default;
+import request from 'supertest';
+import app from '../index';
 
 
 describe('POST /posts', () => {
