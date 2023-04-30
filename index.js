7a687bd3b88511119db9115b9d3b613b1733f66e
mianmiantea2019 <sguo321@gatech.edu>
2023-04-29 23:29:46 -0400

    add comment

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
-import express, { json } from 'express';
+import express from 'express';
+import { json } from 'body-parser';
+import { randomBytes } from 'crypto';
 
 const app = express();
 app.use(json());
 
-const db = [];
+const commentsByPostId = {};
 
-app.post('/posts', (req, res) => {
-  const { title } = req.body;
-  if (!title || title.trim() === '') {
-    return res.status(400).json({ error: 'Title is required' });
-  }
-  const id = Date.now().toString();
-  const post = {
-    id,
-    title
-  };
-  db.push(post);
-  return res.status(201).send(post);
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
   const commentId = randomBytes(4).toString('hex');
-
   const { content } = req.body;
-
   const comments = commentsByPostId[req.params.id] || [];
-
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
-  
   res.status(201).send(comments);
 });
 
-
-export default app;
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
