ee9c434ef08b2f1696203a1451a5e25533cd63c3
mianmiantea2019 <sguo321@gatech.edu>
2023-04-30 10:38:46 -0400

    setup query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -22,9 +23,9 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log(err.message);
-  });
+  // axios.post("http://localhost:4003/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
   res.send({ status: "OK" });
 });
 
